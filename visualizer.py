0f611d3e0b8a02a4df88fd4a67694d66a4b4a927
Cristian Perez Jensen <cristianpjensen@gmail.com>
2020-05-19 13:24:19 +0200

    Implemented buttons and animate.

--- visualizer.py
+++ visualizer.py
@@ -15,12 +15,16 @@ class Visualizer():
         self.COLOUR_EXPLORED = "#152238"
         self.COLOUR_PATH = "#cd8d00"
 
-        self.squares = []
-        self.square_types = []
-        self.start_types_coords = None
-        self.start_coords = None
+        self.squares = list()
+        self.square_types = list()
+        self.buttons = list()
 
         self.window = Tk()
+        self.window.title("Pathfinding Visualizer")
+
+        self.top = Frame(self.window)
+        self.top.pack(side=TOP)
+
         self.canvas = Canvas(
             self.window, width=self.WIDTH, height=self.HEIGHT
         )
@@ -33,24 +37,26 @@ class Visualizer():
 
         self.window.bind("<Button-2>", self.colour_start_goal)
 
+        self.create_button("A*")
+        self.create_button("Dijkstra")
+        self.create_button("D*")
+
         self.window.mainloop()
 
     def colour_wall(self, event):
+        '''Colours a wall, based on the x and y values of the mouse.'''
         x = event.x // self.CELLWIDTH
         y = event.y // self.CELLHEIGHT
+
+        if x < 0 or y < 0:
+            return
 
         self.canvas.itemconfig(
             self.squares[x][y], fill=self.COLOUR_WALL, outline=self.COLOUR_WALL)
         self.square_types[x][y] = 1
 
     def colour_start_goal(self, event):
-        # If there already is a start, remove it.
-        # for row_ind, row in enumerate(self.square_types):
-        #     for col_ind, column in enumerate(row):
-        #         if column == 2:
-        #             self.square_types[row_ind][col_ind] = 0
-        #             self.canvas.itemconfig(
-        #                 self.squares[row_ind][col_ind], fill=self.COLOUR_FREE, outline=self.COLOUR_FREE)
+        '''Colours the start or the end goal, based on the x and y values of the mouse.'''
 
         start = False
         end = False
@@ -65,6 +71,9 @@ class Visualizer():
         x = event.x // self.CELLWIDTH
         y = event.y // self.CELLHEIGHT
 
+        if x < 0 or y < 0:
+            return
+
         if start:
             if end:
                 return
@@ -78,6 +87,7 @@ class Visualizer():
             self.square_types[x][y] = 2
 
     def create_grid(self):
+        '''Creates the grid, based on the constants already declared.'''
         for column in range(self.COLUMNS):
             self.squares.append(list())
             self.square_types.append(list())
@@ -91,6 +101,47 @@ class Visualizer():
                     x1, y1, x2, y2, fill=self.COLOUR_FREE, outline=self.COLOUR_FREE
                 ))
                 self.square_types[column].append(0)
+
+    def create_button(self, algorithm):
+        '''Creats a button, which is linked to the corresponding algorithm.'''
+        self.buttons.append(ttk.Button(
+            self.window, text=algorithm, command=lambda: self.pathfind(
+                algorithm)
+        ).pack(in_=self.top, side=LEFT))
+
+    def pathfind(self, algorithm):
+        '''Picks the pathfinding algorithm used.'''
+        if algorithm == "A*":
+            self.worker = self.a_star()
+        elif algorithm == "Dijkstra":
+            self.worker = self.dijkstra()
+        elif algorithm == "D*":
+            self.worker = self.d_star()
+
+        self.animate()
+
+    def animate(self):
+        '''Animates the pathfinding.'''
+        if self.worker != None:
+            try:
+                next(self.worker)
+                self.window.after(self.SPEED, self.animate)
+            except StopIteration:
+                self.worker = None
+            finally:
+                self.window.after_cancel(self.animate)
+
+    def a_star(self):
+        # TODO
+        return
+
+    def dijkstra(self):
+        # TODO
+        return
+
+    def d_star(self):
+        # TODO
+        return
 
 
 if __name__ == "__main__":
